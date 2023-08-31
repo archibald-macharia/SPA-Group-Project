90b6d71c402b4d6d430b07a9299ca1b21ac9691a
Archibald Macharia Gacheru <archibald.gacheru@student.moringaschool.com>
2023-08-31 11:55:35 +0300

    Navbar initial changes

--- car-wow/src/NavBar.js
+++ car-wow/src/NavBar.js
@@ -1,1 +1,21 @@
 /*Archie*/
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div id = "header">
+            <div id='logo'></div>
+            <div id='navbar'>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </div>
+    );
+
+    
+};
+
+export default NavBar;
